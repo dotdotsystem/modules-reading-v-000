b885098cf527d6c8d00e285110c8b40f2f4d09e2
Claire Harmer <claire.f.harmer@gmail.com>
2018-09-13 20:15:32 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,6 +3,16 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
+
+
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+  include Dance
+  extend MetaDancing
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
 end
