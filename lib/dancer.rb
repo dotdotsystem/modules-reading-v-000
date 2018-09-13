33198519217dd49c79ba39c04d80ba7f1ad2df1f
Claire Harmer <claire.f.harmer@gmail.com>
2018-09-13 20:08:00 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,7 +1,13 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
 
 class Dancer
+
+  class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
   include Dance
   extend MetaDancing
 
